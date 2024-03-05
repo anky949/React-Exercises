b8f1486fc66fe316bbc1a6da5809ba46be410748
ankitgupta <ankit.gupta2@tothenew.com>
2024-03-05 22:54:33 +0530

    added routing

--- week2/src/App.js
+++ week2/src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import {ProductComponent} from "./components/ProductComponent";
+import {
+    BrowserRouter,
+    Route,
+    Routes
+} from "react-router-dom";
+import React from "react";
+import {HomeComponent} from "./components/HomeComponent";
+import Layout from "./components/Layout";
 
 function App() {
+
   return (
+
     <div className="App">
-     <ProductComponent/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<HomeComponent />} />
+                    <Route path="products" element={<ProductComponent />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
